9a863c9e0e584167c7c583b3aab1f41639466c62
unknown <sreeragriyer@gmail.com>
2022-04-20 00:12:39 -0400

    world map

--- js/world_map.js
+++ js/world_map.js
@@ -1,10 +1,37 @@
-console.log("data log")
-//https://www.dropbox.com/scl/fo/mxrg82kxwgw3vrges58l0/h?dl=0&rlkey=u8y2qvtjc2m4p00a5aaiohsiz
-let data = null
-d3.csv("https://www.dropbox.com/s/voljxe8cfc061a1/global_food_prices.csv?dl=0").then((d) => {
-        data = d;
-        console.log("in");
-        console.log(data)
+async function getData() {
+    try {
+        let data = await d3.csv("https://raw.githubusercontent.com/sreeragiyer/GFP-visualizer/main/data/gfp_sampled.csv");
+        return data;
     }
-)
-console.log(data);
+    catch(ex) {
+        console.log("error while fetching data: "+ex);
+        return null;
+    }
+
+}
+
+getData().then((data) => {
+    if(data==null) 
+        return;
+    // set the dimensions and margins of the graph
+    const margin = { top: 5, right: 5, bottom: 5, left: 5 },
+    width = document.querySelector("body").clientWidth,
+    height = 500;
+
+    const svg = d3.select("#d3_demo").attr("viewBox", [0, 0, width, height]);
+
+    let projection = d3.geoEquirectangular().center([0, 0]);
+
+    const pathGenerator = d3.geoPath().projection(projection);
+
+    let g = svg.append("g");
+
+    d3
+    .json(
+        "https://raw.githubusercontent.com/iamspruce/intro-d3/main/data/countries-110m.geojson"
+    )
+    .then((data) => {
+        g.selectAll("path").data(data.features).join("path").attr("d", pathGenerator);
+    });
+
+});
